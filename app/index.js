bb40fb46ed55769a0d4e6267854be6d1ed233e26
Ricardo E. Galvez <galvez667@gmail.com>
2020-05-13 07:51:17 -0400

    :meat_on_bone: some futzing around and progess on a small level:
    * added in some goals to the actual page
    * using parcel to run the project
    	* though using browser sync might still be more useful for testing across devices & network
    * found a solution to the svg clip mask issue viz safari and chrome (webkit & blink browsers)
    	* one must use an `<image>` as child of the `svg` and not an `html` `<img>`
    	* still, more work and explorations need doing

--- app/index.js
+++ app/index.js
@@ -1,3 +1,38 @@
 
+// SVG <stop ></stop>
+// const stopElement = document.querySelector('[offset]');
+const stopElement = document.querySelectorAll('[offset]');
+const containerElements = document.querySelectorAll('.container');
+console.log(stopElement);
 
-const stop = document.querySelector('[offset]');;
+import colorable from 'colorable';
+
+//import { colorable } from 'colorable';
+
+var colors = {
+  red: 'red',
+  green: 'green',
+  blue: 'blue',
+  darkBlue: '#345678',
+  white: '#fff'
+};
+var options = {
+  compact: true,
+  threshold: 0 
+};
+var result = colorable(colors, options);
+
+// var resultObjectAsArray = result.forEach(color => console.log(color.combinations));
+
+// now write some logic here to map your accessible colors to the backgrounds of each container...
+
+// grab the containers
+
+// console.log(containerElements.style);
+
+
+// const containerBackGrounds = containerElements.forEach(container => container.style.backgroundColor = '#fa2c3f');
+
+
+
+
